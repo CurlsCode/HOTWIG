972f4ec157e4064834b69623af358c733ab15689
CurlsCode <carlbiewer@gmail.com>
2026-02-16 08:30:17 +1100

    Add React frontend scaffolding

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>HOTWIG: History of the World I Guess</h1>
+      <p>Welcome to the interactive historical atlas</p>
+    </div>
+  );
+}
+
+export default App;
